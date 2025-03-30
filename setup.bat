803d25083804550f7169bbfac819c8c4b51cfc52
Onhalar <halaczondrej@gmail.com>
2025-03-30 08:44:01 +0200

    added repo setup files

---
+++ setup.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/Onhalar/3DviewerOpenGL.git
+cd 3DviewerOpenGL
+mkdir build
+cd build
+cmake -G "MinGW Makefiles" ..
+cmake --build .
+."/bin/simulacrum.exe"
---
+++ setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Onhalar/3DviewerOpenGL.git
+cd 3DviewerOpenGL
+mkdir build
+cd build
+cmake ..
+make
+./bin/simulacrum
